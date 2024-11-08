5072280902e616513acdece135645de35b16dfb8
lk-beep <prog10695@gmail.com>
2024-11-08 09:20:36 +0200

    fixed all the p classes from changing in cat facts

--- aaapi.js
+++ aaapi.js
@@ -8,14 +8,17 @@ function catfact() {
         headers: headers,
         redirect: 'follow'
     };
+
     const pas = document.querySelectorAll("p");
     pas.forEach((p, index) => {
-        fetch("https://catfact.ninja/fact?max_length=140", requestOptions)
-            .then(response => response.json())
-            .then(result => {
-                const catFact = result.fact;
-                p.textContent = `Cat Fact ${index + 1}: ${catFact}`;
-            })
-            .catch(error => console.log('Error:', error));
+        if (!p.classList.contains("dintchang")) {
+            fetch("https://catfact.ninja/fact?max_length=140", requestOptions)
+                .then(response => response.json())
+                .then(result => {
+                    const catFact = result.fact;
+                    p.textContent = `Cat Fact ${index - 1}: ${catFact}`;
+                })
+                .catch(error => console.log('Error:', error));
+        }
     });
 }
